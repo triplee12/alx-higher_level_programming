a850545e4042272bb4770ed8f121d376c9d3fa18
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-12-01 08:35:19 -0800

    Fix code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     Follows by argument index number and the name.
     """
     if len(argv) == 1:
-        print("0 arguments.")
+        print("0 arguments", end=".\n")
     elif len(argv) == 2:
-        print("{} argument:".format(len(argv) - 1))
+        print("{} argument".format(len(argv) - 1), end=":\n")
         for i in range(len(argv) - 1):
             print("{}: {}".format(i + 1, argv[i + 1]))
     else:
-        print("{} argument:".format(len(argv) - 1))
+        print("{} arguments".format(len(argv) - 1), end=":\n")
         for i in range(len(argv) - 1):
             print("{}: {}".format(i + 1, argv[i + 1]))
