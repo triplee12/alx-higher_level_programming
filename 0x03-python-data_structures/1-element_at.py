d8046efd1c102cf39038ccaae15889a4f11c7c5a
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-12-05 03:57:40 -0800

    Fix code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     """Access element at a given index"""
-    if idx < 0:
-        return None
-    elif idx > len(my_list):
+    if idx < 0 or idx > len(my_list) - 1:
         return None
     else:
         return my_list[idx]
