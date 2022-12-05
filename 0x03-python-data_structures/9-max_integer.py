a4b5ea14fef5c08cf7b6d2a07b8cfd40f0c359f3
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-12-05 04:29:22 -0800

    Fix code

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,10 +3,10 @@
 def max_integer(my_list=[]):
     """Find the biggest integer of a list"""
     if len(my_list) == 0:
-        return(None)
+        return None
     else:
         maximum = 0
         for i in my_list:
             if i > maximum:
                 maximum = i
-        return(maximum)
+        return maximum
