f80d947c14c57226911cd2198fa854e18a2ab849
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-12-05 00:09:19 -0800

    Fix code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,14 @@
 
 def print_matrix_integer(matrix=[[]]):
     """Print a matrix of integers"""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{}".format(matrix[i][j]), sep='', end=" ")
-        print(sep=' ')
+    if not matrix:
+        print()
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j != len(matrix[i]) - 1:
+                    end = ' '
+                else:
+                    end = ''
+                print("{:d}".format(matrix[i][j]), end=end)
+            print()
