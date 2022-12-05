fbb569b6af4b2a9ef9bc186cea1e5661de4961f6
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-12-05 02:06:09 -0800

    Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """Find the biggest integer of a list"""
+    if len(my_list) == 0:
+        return(None)
+    else:
+        maximum = 0
+        for i in my_list:
+            if i > maximum:
+                maximum = i
+        return(maximum)
