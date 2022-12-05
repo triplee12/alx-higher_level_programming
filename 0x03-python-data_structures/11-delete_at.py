288211d974a36b4f8cf6591c26a01cf153db1ef5
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-12-05 02:30:18 -0800

    Delete at a given index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    """Delete an element at a given index of a list"""
+    if idx < 0 or idx > len(my_list):
+        return(my_list)
+    else:
+        del(my_list[idx])
+        return(my_list)
