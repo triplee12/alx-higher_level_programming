a05ef73d6481067281b3c82d3a592e1da72c61d0
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-01-09 05:01:09 -0800

    Fix code

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,9 +7,6 @@ class MyList(list):
     """ MyList class
     """
 
-    def __init__(self):
-        super().__init__()
-
     def print_sorted(self):
         """ print_sorted prints the list, but sorted
         (ascending sort)
