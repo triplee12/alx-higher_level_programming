6e0c1dcce9b884f2290841e1ec1237f07bdb5484
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-01-09 07:08:14 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class
+    """
+
+    def area(self):
+        """Area function not implemented yet
+        """
+
+        raise Exception("area() is not implemented")
