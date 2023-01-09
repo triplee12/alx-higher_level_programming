90e605a7513c095facf8e93a3ae3bd6a3f167e39
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-01-09 04:10:05 -0800

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """Returns the list of available attributes and methods
+    of an object.
+    Args:
+        obj (class): The object to lookup.
+    """
+    return dir(obj)
