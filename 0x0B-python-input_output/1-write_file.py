46d8c48020f3d646e1b2456b0c691d36e55a37b3
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-01-10 11:30:27 -0800

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -13,3 +13,9 @@ def write_file(filename="", text=""):
     """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
+        line_num = 0
+
+        for line in f:
+            line_num += 1
+
+        return line_num
