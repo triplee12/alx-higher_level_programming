d080e447a3f2d4b20a3001171f8ad20347b7a1f9
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-01-10 13:39:36 -0800

    Fix import error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,10 +5,8 @@ import sys
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
 try:
     my_list = load_from_json_file("add_item.json")
 except FileNotFoundError:
     my_list = []
     save_to_json_file(my_list + sys.argv[1:], "add_item.json")
-
