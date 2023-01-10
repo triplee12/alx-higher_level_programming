9fe1f2a95ad0ad298d9b6067dbd885fc618dc424
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-01-10 11:05:26 -0800

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""read_file"""
+
+
+def read_file(filename=""):
+    """Read file and prints it to stdout
+
+    Args:
+        filename (str): file name to read
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        f.read()
