4bbfc9511926b84a72f214413b653be2f7fc8c5b
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-01-10 11:22:34 -0800

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""write_file module"""
+
+
+def write_file(filename="", text=""):
+    """Write string to a text file and returns the number
+    of characters written
+
+    Args:
+        filename (str): file name of the file to write
+        text (str): the string to write in the file
+
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
