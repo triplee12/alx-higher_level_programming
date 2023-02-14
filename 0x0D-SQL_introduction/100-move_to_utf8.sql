d32fd5bc6582390cd552715673fe575a68b868bc
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-14 05:36:44 -0800

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Alter the charset coding in a the database hbtn_0c_0
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Alter the charset coding in a the table hbtn_0c_0.first_table
+ALTER TABLE `hbtn_0c_0`.`first_table` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Alter the charset coding in a the table hbtn_0c_0.first_table.name
+ALTER TABLE `hbtn_0c_0`.`first_table`
+MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
