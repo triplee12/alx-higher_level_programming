aaac46fd0e002b713743881615b057a8ab4cad80
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-14 05:42:23 -0800

    Temperatures #1

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Select temperature avergae by city from hbtn_0c_0.temperatures in summer
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month=7 or month=8
+GROUP BY city
+ORDER BY AVG(value) DESC
+LIMIT 3;
