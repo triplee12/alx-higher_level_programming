67cb5c0785be37fc50185598706cf4251fd33a4b
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-14 05:12:42 -0800

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,15 @@
+-- Create table hbtn_0c_0.second_table
+CREATE TABLE IF NOT EXISTS `second_table`(
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+
+-- Insert multiple records to hbtn_0c_0.second_table
+INSERT INTO `second_table`
+    (`id`, `name`, `score`)
+VALUES
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
