02c16020f5add04d59d4e51a5fb790405f1e1920
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-15 08:24:15 -0800

    List shows and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Lists all shows, and all genres linked to it
+SELECT      s.`title`, g.`name`
+FROM        `tv_shows` AS s
+            LEFT JOIN `tv_show_genres` AS sg
+                ON s.`id` = sg.`show_id`
+                LEFT JOIN `tv_genres` AS g
+                    ON sg.`genre_id` =  g.`id`
+ORDER BY    s.`title` ASC,
+            g.`name` ASC;
