3bb204d75ce887c5658a649913b536686076b301
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-15 07:16:39 -0800

    Read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Creates the database hbtn_Od_2
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+-- Create user user_0d_2@localhost
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+            IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant previleges to the user
+GRANT SELECT
+          ON `hbtn_0d_2`.*
+          TO 'user_0d_2'@'localhost';
