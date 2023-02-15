05671282be0561efb6c6e370712cb183c8dbcfda
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-15 04:21:39 -0800

    My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- Lists all previleges of the user user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- Lists all previleges of the user user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
