fbc357fae19b78381d2fcec4d5d2b81f41ed75ab
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-15 08:02:17 -0800

    Cities of California

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Lists all cities in California
+SELECT DISTINCT `cities`.`id`, `cities`.`name`
+FROM            `cities`, `states`
+WHERE           `cities`.`state_id` =
+(
+    SELECT  `states`.`id`
+    FROM    `states`
+    WHERE   `states`.`name` = "California"
+)
+ORDER BY    `cities`.`id` ASC;
