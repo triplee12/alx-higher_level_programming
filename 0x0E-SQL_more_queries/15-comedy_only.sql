305658d74fbd7bb821330d8cc3cfb3aec9c077bb
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2023-02-15 08:22:07 -0800

    Only Comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- Lists all comedy shows
+SELECT      s.`title`
+FROM        `tv_shows` AS s
+            INNER JOIN `tv_show_genres` AS sg
+                ON s.`id` = sg.`show_id`
+                INNER JOIN `tv_genres` AS g
+                    ON sg.`genre_id` =  g.`id`
+WHERE       g.`name` = 'Comedy'
+ORDER BY    s.`title` ASC;
